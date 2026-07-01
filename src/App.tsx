6619ff44021852ba80371bb217a3bf4815277e06
Maher Al Rifai <maher.alrifai@ogilvy.com>
2026-07-01 11:49:16 +0300

    Add Maher's Components

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import OpportunitiesSection from "./components/OpportunitiesSection/Opportunitie
 import NavbarSection from './components/NavbarSection'
 import TaleedOpportunities from './components/TaleedOpportunities'
 import ServicesSection from './components/ServicesSection'
+import { FocusAreas } from './components/FocusAreas/FocusAreas'
+import { ImpactStats } from './components/ImpactStats/ImpactStats'
+import { TaleedFooter } from './components/Footer/TaleedFooter'
 
 function App() {
   return (
@@ -13,11 +16,11 @@ function App() {
       <HeroSection />
       <TaleedOpportunities />
       <ServicesSection />
- 
-
       <OpportunitiesSection />
+      <ImpactStats />
+      <FocusAreas />
       <PartnersSection />
-
+    <TaleedFooter />
     </main>
   );
 }
